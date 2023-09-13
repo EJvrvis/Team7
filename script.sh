9a51f6c0e5ae31b57bfb5ed2ba849dcea28ed363
Esele B. Jarvis <eseleb.jarvis@Eseles-MBP.lan>
2023-09-13 02:55:45 -0400

    my first commmit

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+#
+echo " Please enter your name: "
+read $name 
+echo " Please enter your team: "
+read $team 
+echo " Welcome $name to Etech Master Class $team. "
+echo "end of script. "
